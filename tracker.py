b69f2b7863ea16762344d528bc65701098e4b929
pioreklolek <piotrorlowski2004@gmail.com>
2026-04-07 00:59:15 +0200

    add Earth-analog star tracking and fix collision detection
    - fix collision  to use galaxy separation instead of andromeda.pos.mag
    
    - add StarTracker class: selects stars at Earth-like orbital radius at step 0
    
    - add is_merged and is_earth_analog flags to Star
    
    - scale up galaxy parameters (radii, thickness, star counts, positions)
    
    - add EARTH_ORBITAL_RADIUS, EARTH_ORBITAL_BAND, COLLISION_THRESHOLD constants

---
+++ tracker.py
@@ -0,0 +1,27 @@
+from globals import *
+
+class StarTracker:
+    #select stars whose orbits at step 0 is close to Earth's , marks them green
+
+    def __init__(self, galaxies, earth_radius, earth_band):
+        #galaxies - list of Galaxy obj to search
+        # earth radius - oribtal radius of Earth-Like (meters), import from Globals
+        #earth_band - tolerance +- to orbital radius (meters) , import from globals 
+        self.earth_analogs = []
+        self._select(galaxies,earth_radius,earth_band)
+        print(f"StarTracker: {len(self.earth_analogs)} Earth-Like stars selected!")
+
+    def _select(self,galaxies,earth_radius,earth_band):
+        for galaxy in galaxies:
+            for star in galaxy.stars:
+                orbital_radius = (star.pos - galaxy.pos).mag
+                if abs(orbital_radius - earth_radius) < earth_band:
+                    star.obj.color = EARTH_COLOR
+                    star.is_earth_analog = True
+                    self.earth_analogs.append(star)
+
+    def record_step(self,step,galaxy_center):
+        pass
+
+    def save_to_file(self,filepath,sim_params):
+        pass
